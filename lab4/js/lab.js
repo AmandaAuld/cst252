bc009e85cbca60f93f112e3a55ad0d7ae80842b7
Amanda Auld <mooauld@gmail.com>
2022-02-08 16:48:28 -0800

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,12 @@
+//author: amanda auld
+//date: 01.08.2022
+//public domain
+
+
+
+//Download photoshop
+  //Open photoshop
+    //Click file>create new
+      //Set document dimensions
+        //Press create
+          //Begin photoshop project
