3698855b614b7d00b71b5c610a2ec048e3848097
Amanda Auld <mooauld@gmail.com>
2022-02-09 12:58:24 -0800

    lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,19 @@
+//author: amanda auld
+//date: 02.09.2022
+//public domain
+
+// Define Variables
+make = "Ford";
+model = "Focus";
+color = "Grey";
+year = "2013";
+
+// Calculate
+age = 2022 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
