0d6f9c5567caef99b90a77dc79d89a82b32f8fcb
Amanda Auld <mooauld@gmail.com>
2022-02-20 17:18:08 -0800

    lab6

---
+++ lab6/js/lab.js
@@ -0,0 +1,22 @@
+//author: amanda auld
+//date: 02.20.2022
+//public domain
+
+//Define Variables
+myTransport = ["Ford Focus", "Monterey-Salinas Transit", "Walking", "Uber"
+];
+
+myMainRide = {
+  make : "Ford"
+  model : "Focus"
+  color : "grey"
+  year : 2013,
+  age : function() {
+    return 2022 - age;
+  }
+}
+
+//output
+document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+document.writeln("My main ride: <pre>",
+JSON.stringify(myMainRide, null, '\t'), "</pre>");
