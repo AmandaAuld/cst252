bd74591015af62e3e56aea9080c3cb8306f3869a
Amanda Auld <mooauld@gmail.com>
2022-02-22 19:18:26 -0800

    Update lab.js

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -3,22 +3,24 @@
 //public domain
 
 
-//sortUserName - a function that  takse a user input and sorts the letters
+//sortUserName - a function user input that sorts the letters
 // of their name
-function sortUserName() {
-    var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
-    console.log("userName =", userName);
-    //split string to array
-    var nameArray = userName.split('');
-    console.log("nameArray =", nameArray);
-    //sort the array
-    var nameArraySort = nameArray.sort();
-    console.log("nameArraySort =", namrArraySort);
-    //join array back to a string
-    var nameSorted = nameArraySort.join('');
-    console.log("nameSorted =", nameSorted);
-    return nameSorted;
 
-    //Output
-    document.writeln("Oh hey! I've fixed your name: ",
-    sortUserName(), "</br>");
+function sortUserName(){
+  var username = window.prompt("Hi! Please tell me your name so I can fix it!");
+  console.log("userName=", userName);
+  //split string to array
+  var nameArray = userName.split('');
+  console.log("nameArray=", nameArray);
+  //sort the array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort=", nameArraySort);
+  //join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted=", nameSorted);
+  return nameSorted;
+}
+
+//Output
+document.writeln("Oh hey! I fixed your name: ",
+sortUserName(), "</br>");
