dc46ca5882cd42f48fd15ac28dcff6ec6106c02d
Amanda Auld <mooauld@gmail.com>
2022-03-01 18:32:40 -0800

    lab9

---
+++ lab9/js/lab.js
@@ -0,0 +1,14 @@
+//author: amanda auld
+//date: 03.01.2022
+//public domain
+
+var targetEl = document.getElementById("output");
+
+var newEl = document.createElement("button");
+
+targetEl.appendChild(newEl);
+
+newEl.innerhtml = "Click me";
+newEl.style.backgroundColor = "red";
+newEl.style.color = "red";
+newEl.style.fontSize = "100px";
